cd15c9ef97744cd618f255ea65c722f531dfed69
Dragos <edragos33@gmail.com>
2025-01-27 09:21:01 +0100

    division added

---
+++ src/Division.java
@@ -0,0 +1,31 @@
+public class Division {
+
+    private int a = 0, b = 0;
+
+    public Division() {}
+
+    public Division(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+    public int getB() {
+        return b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public String divide(int a, int b) {
+        int sum = a / b;
+        return "Result is: " + sum;
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,14 @@ public class Main {
 
         Addition add = new Addition();
         Substract sub = new Substract();
+        Multiply mul = new Multiply();
+        Division div = new Division();
 
 
         System.out.println(add.addition(66, 8));
         System.out.println(sub.substraction(55, 80));
+        System.out.println(mul.multiply(3,6));
+        System.out.println(div.divide(8,8));
 
     }
 }
