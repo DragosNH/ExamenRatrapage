e21f5ba9ffb2faffd20b986bfd8bbc989fd4f76c
Dragos <edragos33@gmail.com>
2025-01-27 09:16:49 +0100

    multiplication added

--- src/Multiply.java
+++ src/Multiply.java
@@ -1,5 +1,31 @@
 public class Multiply {
 
+    private int a = 0, b = 0;
 
+    public Multiply() {}
+
+    public Multiply(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+
+    public int getA() {
+        return a;
+    }
+    public int getB() {
+        return b;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+    public void setB(int b) {
+        this.b = b;
+    }
+
+    public String multiply(int a, int b) {
+        int sum = a * b;
+        return "Result is: " + sum;
+    }
 
 }
