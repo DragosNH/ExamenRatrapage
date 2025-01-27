7c5370cf171c056fa5225ce592a80005484d6fd3
Dragos <edragos33@gmail.com>
2025-01-27 09:15:35 +0100

    Substraction added

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,11 @@ public class Main {
     public static void main(String[] args) {
 
         Addition add = new Addition();
+        Substract sub = new Substract();
 
 
         System.out.println(add.addition(66, 8));
+        System.out.println(sub.substraction(55, 80));
 
     }
 }
